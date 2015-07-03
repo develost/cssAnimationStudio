163ca9fc881881877344c23cf861a70aefc8c662
develost.com <info@develost.com>
2015-07-03 16:25:07 +0200

    reorganize  code

--- js/keyframesAnimationStudio.js
+++ js/keyframesAnimationStudio.js
@@ -1,8 +1,9 @@
-
 $(init);
-
 function init() {
 
+    // ========================================================================
+    // Utility functions
+    // ========================================================================
     function pad(pad, str, padLeft) {
         if (typeof str === 'undefined') return pad;
         if (padLeft) {
@@ -11,48 +12,40 @@ function init() {
             return (str + pad).substring(0, pad.length);
         }
     }
-
-    var nSteps = 36;
-    var currentStep = 16;
+    
+    // ========================================================================
+    // Global variables
+    // ========================================================================
+    var nSteps = 3;
+    var currentStep = 1;
     var status = [];
     
+    // ========================================================================
+    // Gui initialization
+    // ========================================================================
     $('.piece').each(function () {
         var pieceId = $(this).attr('id');
         status[pieceId] = 0;
     });
-
     $('.piece').draggable();
     $('.messages').draggable();
-    
-    
-    
-    //$('#message1').text(welcomeMessage);
     $('#message1').empty().append(getWelcomeMessage());
     checkConnect();
     
-    $('body').bind( "click", function( event) {
-        $('#message1').empty().append(getWelcomeMessage());
-
-    });    
-    
-    $('.commands').bind( "click", function( event) {
-        var commandId = $(this).attr('id');
-        $('#message1').empty().append(commandId);
-        return false;
-    });      
-    
-    
-    
+    // ========================================================================
+    // Update UI
+    // ========================================================================
     function getWelcomeMessage(){
         var welcomeMessage = "";
         welcomeMessage += "==================================\n";
         welcomeMessage += "=   Keyframes Animation Studio   =\n";
         welcomeMessage += "==================================\n";
         welcomeMessage += "=                                =\n";
-        welcomeMessage += "= nSteps      :" + pad("   ",nSteps,true) + "       <a class=\"commands\" id=\"lessSteps\">(-)</a> <a class=\"commands\" id=\"moreSteps\">(+)</a> =\n";
-        welcomeMessage += "= currentStep :" + pad("   ",currentStep,true) + "       <a class=\"commands\" id=\"prevStep\">(-)</a> <a class=\"commands\" id=\"nextStep\">(+)</a> =\n";
+        welcomeMessage += "= nSteps      :" + pad("   ",nSteps,true) + "       <a class=\"commands lessSteps\">(-)</a> <a class=\"commands moreSteps\">(+)</a> =\n";
+        welcomeMessage += "= currentStep :" + pad("   ",currentStep,true) + "       <a class=\"commands prevStep\">(-)</a> <a class=\"commands nextStep\">(+)</a> =\n";
         welcomeMessage += "=                                =\n";
-        welcomeMessage += "=         <a class=\"commands\" id=\"export\">export</a>  <a class=\"commands\" id=\"import\">import</a>         =\n";
+        welcomeMessage += "=  <a class=\"commands\" >copyprev</a> <a class=\"commands\" >deletecurr</a> <a class=\"commands\" >copynext</a>  =\n";
+        welcomeMessage += "=   <a class=\"commands\" >save</a> <a class=\"commands\" >discard</a> <a class=\"commands\">export</a> <a class=\"commands\">import</a>   =\n";
         welcomeMessage += "=                                =\n";
         welcomeMessage += "==================================\n";
         welcomeMessage += "=                                =\n";
@@ -63,7 +56,7 @@ function init() {
         welcomeMessage += "=== by <a href=\"http://www.develost.com\">develost.com</a> ==============";
         return welcomeMessage;
     }
-    
+
     function updateMessageElement(element){
         var pieceId = $(element).attr('id');
         var offset  = $(element).offset();
@@ -90,25 +83,12 @@ function init() {
             message += "=                                =\n";
         });
         message += "=== by ";
-        //$('#message1').text(message);
         $('#message1').empty().append(message + "<a href=\"http://www.develost.com\">develost.com</a> ==============");
-        
     }
     
-    function updateConnectionPoints(){
-        var message = "";
-        $('.conn').each(function () {
-            var connId = $(this).attr('id');    
-            var offset  = $(this).offset();
-            message += "ConnId: " + connId + "\n";
-            message += "Top: " + offset.top + "px\n";
-            message += "Left: " + offset.left + "px\n";
-            message += "-----------------\n";
-        });
-        $('#message2').text(message);
-    }
-    
-    
+    // ========================================================================
+    // Methods for pieces
+    // ========================================================================
     function checkConnect(){
         $('.conn').removeClass("conn-near conn-exact conn-same").addClass("conn-disconnected");    
         $('.conn').each(function () {
@@ -125,7 +105,6 @@ function init() {
                     $(currentConn).removeClass("conn-near conn-exact conn-same conn-disconnected").addClass("conn-near");
                     $(this).removeClass("conn-near conn-exact conn-same conn-disconnected").addClass("conn-near");
                 }
-                
                 if  (( Math.abs(testTop-currentTop) < 5) &&  ( Math.abs(testLeft-currentLeft) < 5 ) && ( currentId != testId )){
                     $(currentConn).removeClass("conn-near conn-exact conn-same conn-disconnected").addClass("conn-exact");
                     $(this).removeClass("conn-near conn-exact conn-same conn-disconnected").addClass("conn-exact");
@@ -136,20 +115,24 @@ function init() {
                 }
             });
         });
+    }    
+   
+    // debug method
+    function updateConnectionPoints(){
+        var message = "";
+        $('.conn').each(function () {
+            var connId = $(this).attr('id');    
+            var offset  = $(this).offset();
+            message += "ConnId: " + connId + "\n";
+            message += "Top: " + offset.top + "px\n";
+            message += "Left: " + offset.left + "px\n";
+            message += "-----------------\n";
+        });
+        $('#message2').text(message);
     }
-    
-    $('.piece').bind( "dragstart drag click", function( event) {
-        updateMessageElement(this);
-        checkConnect();
-        //This is for debug only
-        //updateConnectionPoints();
-        event.stopPropagation();
-    });
-    
-    $('.piece').bind( "dragstop", function( event) {
-        updateMessageElement(this);
-        
-        $(this).children('.conn').each(function () {
+
+    function connectNear(element){
+        $(element).children('.conn').each(function () {
             var currentTop = $(this).offset().top;
             var currentLeft = $(this).offset().left;
             var currentId = $(this).attr('id');
@@ -163,26 +146,17 @@ function init() {
                     var parentOffset = $(currentConn).parent().offset();
                     var deltaTop = currentTop-testTop;
                     var deltaLeft = currentLeft-testLeft;
-                    
                     $(currentConn).parent().offset({top: parentOffset.top-deltaTop , left:parentOffset.left-deltaLeft});
-                    
-                    //$(currentConn).removeClass("conn-disconnected").addClass("conn-connected");
-                    //$(this).removeClass("conn-disconnected").addClass("conn-connected");
                 }
             });            
         });
-        checkConnect();
-        //This is for debug only
-        //updateConnectionPoints();
-        event.stopPropagation();
-    });    
+    }
     
-    $('.piece').bind('mousewheel DOMMouseScroll', function(event){
-        var pieceId = $(this).attr('id');
+    function rotate(element,event){
+        var pieceId = $(element).attr('id');
         var currentStatus = status[pieceId];
         var nextStatus = 0;
         var statusInverted = 0;
-
         if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
             nextStatus = currentStatus + 5;
             if (nextStatus >= 360) {nextStatus = 0;}
@@ -192,18 +166,68 @@ function init() {
         }
         var currentStatusInverted = 360-currentStatus;
         var nextStatusInverted = 360-nextStatus;
-        $(this).removeClass("rotate"+currentStatus).addClass("rotate"+nextStatus );
-        
-        $(this).children().removeClass("rotate"+currentStatusInverted).addClass("rotate"+nextStatusInverted );
-        
-        status[pieceId] = nextStatus;
-        
+        $(element).removeClass("rotate"+currentStatus).addClass("rotate"+nextStatus );
+        $(element).children().removeClass("rotate"+currentStatusInverted).addClass("rotate"+nextStatusInverted );
+        status[pieceId] = nextStatus;    
+    }
+    
+    // ========================================================================
+    // Event binding
+    // ========================================================================
+    $('body').bind( "click", function( event) {
+        $('#message1').empty().append(getWelcomeMessage());
+    });    
+    
+    $( document ).delegate( "a.lessSteps", "click", function() {
+        if ( nSteps == 1 ) {return;};
+        if (confirm("This will delete permanently step number " + nSteps + ". \nContinue?")){
+            nSteps--;
+        }
+        $('#message1').empty().append(getWelcomeMessage());
+        return false;
+    });
+
+    $( document ).delegate( "a.moreSteps", "click", function() {
+        nSteps++;
+        $('#message1').empty().append(getWelcomeMessage());
+        return false;
+    });
+    
+    $( document ).delegate( "a.prevStep", "click", function() {
+        currentStep--;
+        if (currentStep < 1 ){currentStep = nSteps;}
+        $('#message1').empty().append(getWelcomeMessage());
+        return false;
+    });    
+
+    $( document ).delegate( "a.nextStep", "click", function() {
+        currentStep++;
+        if (currentStep > nSteps ){currentStep = 1;}
+        $('#message1').empty().append(getWelcomeMessage());
+        return false;
+    });
+    
+    $('.piece').bind( "dragstart drag click", function( event) {
+        updateMessageElement(this);
+        checkConnect();
+        //updateConnectionPoints();
+        event.stopPropagation();
+    });
+    
+    $('.piece').bind( "dragstop", function( event) {
+        updateMessageElement(this);
+        connectNear(this);
+        checkConnect();
+        //updateConnectionPoints();
+        event.stopPropagation();
+    });    
+    
+    $('.piece').bind('mousewheel DOMMouseScroll', function(event){
+        rotate(this,event);
         updateMessageElement(this);
         checkConnect();
         event.stopPropagation();
-        //This is for debug only
         //updateConnectionPoints();
-        
     });
     
 } 
